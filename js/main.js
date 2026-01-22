a7de23db512d1d69d41e786815e8a3450fae53da
Crisraccesories <cristianmfc54@gmail.com>
2026-01-22 00:40:21 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-const whatsFloat = document.getElementById('whatsFloat');
-if (whatsFloat){
-  whatsFloat.addEventListener('click', ()=>{
-    const url = 'https://wa.me/573133067667?text=' + encodeURIComponent('Hola, vengo del sitio de Ancol One. Necesito ayuda.');
-    window.open(url, '_blank');
-  });
-}
